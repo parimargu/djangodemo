4e6245120b51cb944a52c46856576aeab2247be9
parimargu <pari.margu2010@gmail.com>
2021-06-02 19:24:51 +0530

    Added home.html page

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# M - models
+# V - views
+# T - templates
+
+
+def home_view(request):
+    hello = "hello world from the view"
+    return render(request, 'sales/home.html', {'hello': hello})
